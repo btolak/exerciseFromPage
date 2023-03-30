66dd5c4eeb0bc47bb1c5306049600a704c17f372
Bartek <bartlomiejtolak7@gmail.com>
2023-03-30 11:20:26 +0200

    [add]add CountPositivesSumNegatives with my solution

---
+++ src/main/java/com/codewars/task5/CountPositivesSumNegatives.java
@@ -0,0 +1,22 @@
+package com.codewars.task5;
+
+
+public class CountPositivesSumNegatives {
+    public static int[] countPositivesSumNegatives(int[] input) {
+
+        if (input == null || input.length < 1) {
+            return new int[]{};
+        }
+        int positives = 0;
+        int negatives = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] > 0) {
+                positives++;
+            } else if (input[i] < 0) {
+                negatives += input[i];
+            }
+        }
+        return new int[]{positives, negatives};
+    }
+}
