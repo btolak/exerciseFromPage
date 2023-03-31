ffce8ff1d59c28e173800b2cfa9cf7495e15c2e1
Bartek <bartlomiejtolak7@gmail.com>
2023-03-31 16:51:28 +0200

    [add ]add CalculateBMI with my solution

---
+++ src/main/java/com/codewars/task7/CalculateBMI.java
@@ -0,0 +1,9 @@
+package com.codewars.task7;
+
+public class CalculateBMI {
+    public static String bmi(double weight, double height) {
+        double result = weight/(height*height);
+
+        return (result<=18.5) ? ("Underweight"):(result<=25) ? ("Normal"): (result<=30)? ("Overweight"):("Obese");
+    }
+}
